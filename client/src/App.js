87e6c8f728738d4e37c7da076c3f78082a0008d8
venu-42 <solipuram42@gmail.com>
2021-10-29 23:56:31 +0530

    search bar & Add btn added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Clubs from './components/Clubs/Clubs';
+import SearchBar from './components/SearchBar/SearchBar';
 function App() {
   return (
     <div className="App">
       <Header />
       <Clubs />
+      {/* <SearchBar placeholder={"Search book"} /> */}
     </div>
   ); 
 }
---
+++ client/src/components/AddBtn/AddBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './AddBtn.scss';
+
+const AddBtn = ({placeholder}) => {
+
+  return (
+    <button className="AddBtn">+</button>
+  )
+};
+
+export default AddBtn;
